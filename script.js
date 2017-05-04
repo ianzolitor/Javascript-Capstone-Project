60880f140b7d8fc60a63bc07cdeffed83fc38851
Ian Zolitor <ianzolitor@gmail.com>
2017-05-04 11:31:24 -0400

    split word function

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-var puzzleArea = document.getElementsByClassName("puzzle")
+var puzzleArea = document.getElementsByClassName("puzzle")[0]
+
+
+
+
+
+
+function makePuzzle (word) {
+	wordArray = []
+	for (var i = 0; i < word.length; i++) {
+    wordArray.push(word.charAt(i))
+    console.log(wordArray)
+	}
+	wordArray.forEach(function(letter){
+		puzzleArea.innerHTML += "<div class = 'puzzle-letter'>" + letter + "</div>"
+	})
+
+}
+
