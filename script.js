519eaf906120359c63710ecae056f2d778b5ac47
Ian Zolitor <ianzolitor@gmail.com>
2017-05-04 14:06:45 -0400

    puzzle is an object

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-var puzzleArea = document.getElementsByClassName("puzzle")[0]
-var guessButton = document.getElementsByClassName("guess-button")[0]
-var guessedLetter = document.getElementById("guessed-letter")
-var puzzleLetter = document.getElementsByClassName("puzzle-letter")
+var puzzleArea = document.getElementsByClassName("puzzle")[0];
+var guessButton = document.getElementsByClassName("guess-button")[0];
+var guessedLetter = document.getElementById("guessed-letter");
+var puzzleLetter = document.getElementsByClassName("puzzle-letter");
+var newButton = document.getElementsByClassName("new-button")[0];
+var sorry = document.getElementsByClassName("sorry")[0]
+
+var wof = new Puzzle;
+var wordArray = [];
 
 
+newButton.addEventListener("mouseover", function (){
+	event.target.style.cursor = "pointer"
+})
 
+newButton.addEventListener("click", makePuzzle)
 
 guessButton.addEventListener("mouseover", function (){
 	event.target.style.cursor = "pointer"
@@ -13,35 +22,54 @@ guessButton.addEventListener("mouseover", function (){
 guessButton.addEventListener("click",guessLetter)
 
 
+wof.loadWord("goat")
+wof.loadWord("applesauce")
+wof.loadWord("computer")
+
+function makePuzzle () {
+	var randomWord = Math.floor((Math.random() * wof.wordBank.length) + 0);
+	word = wof.wordBank[randomWord]
+	for (var i = 0; i < word.length; i++) {
+	    wordArray.push(word.charAt(i))
+	    console.log(wordArray)
+		}
+		wordArray.forEach(function(letter){
+			puzzleArea.innerHTML += "<div class = 'puzzle-letter-box'> <div class = 'puzzle-letter'>" + letter + "</div> </div>"
+		})
+
+	}
+
 function guessLetter () {
 	for (var i = 0; i < wordArray.length; i++) {
 		if (guessedLetter.value === wordArray[i]) {	
 			puzzleLetter[i].style.display = "block"
+			sorry.innerHTML = ""
 			}
 		else {
-		console.log("you fail")
+		sorry.innerHTML = "Sorry, there are no " + guessedLetter.value + "'s"
 			}
 	}	
 }
-wordArray = []
-function makePuzzle (word) {
-	for (var i = 0; i < word.length; i++) {
-    wordArray.push(word.charAt(i))
-    console.log(wordArray)
-	}
-	wordArray.forEach(function(letter){
-		puzzleArea.innerHTML += "<div class = 'puzzle-letter-box'> <div class = 'puzzle-letter'>" + letter + "</div> </div>"
-	})
+
+function clearPuzzle () {
+	puzzleArea.innerHTML = ""
 
 }
 
+
 function Word(word) {
-this.word = word
+
 }
 
 
 function Puzzle() {
 this.wordBank = [];
-this.makePuzzle = MakePuzzle;
+this.loadWord = loadWord;
+this.makePuzzle = makePuzzle;
 
+
+
+	function loadWord (word) {
+	this.wordBank.push(word)
+	}
 }
